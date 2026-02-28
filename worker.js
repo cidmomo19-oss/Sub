926b66fb3b5313d45bd20a070ee568dbc3101766
Okienak <cidmomo19@gmail.com>
2026-03-01 04:13:52 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ const CONFIG = {
   YT_SUB: "https://www.youtube.com/@seputarlinkvidey",
   
   // 2. Video Like & Komen (GANTI LINK INI JIKA ADA VIDEO BARU)
-  YT_VIDEO: "https://youtu.be/53DSz2PB-Qw?si=sHkZghU60C4Ta8tK",
+  YT_VIDEO: "https://youtu.be/0MWWKiCZmqs?si=qeGMZuoYy974H4Hr",
   
   // 3. Saluran WhatsApp
   WA_LINK: "https://whatsapp.com/channel/0029Vb6hCJ7CBtxHIx0Xyc0N"
